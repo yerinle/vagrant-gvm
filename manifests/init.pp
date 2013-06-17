99525bab13fd522d53ba58057c2c8ac2c17e67b2
Yinka Erinle <yerinle@gmail.com>
2013-06-17 23:23:18 +0100

    Now successfully installs gvm and java

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,14 @@
 include gvm
+
+exec { "apt-get update":
+      command => "apt-get update",
+      cwd => "/var/tmp",
+      path    => ["/usr/bin", "/usr/sbin"],
+      logoutput => true,
+      before => Class['java']
+}
+
+class { 'java':
+  distribution => 'jdk',
+  version      => 'latest',
+}
--- modules/gvm/manifests/system.pp
+++ modules/gvm/manifests/system.pp
@@ -1,22 +1,12 @@
 # install gvm
 class gvm::system {
 
-	$home_sh = "/etc/profile.d/home.sh"
-	file { $home_sh:
-	  ensure => present,
-	  content => "export HOME=/home/vagrant",
-	  owner => "vagrant",
-	  group => "vagrant",
-	}
-	
 	exec { "install-gvm":
-		command => "curl -s get.gvmtool.net | bash",
+		command => "sudo su - vagrant -c 'curl -s get.gvmtool.net | bash'",
 		cwd => "/var/tmp",
 		user => "vagrant",
 		logoutput => true,
 		path => ["/usr/bin", "/usr/sbin", "/bin"],
-		require => File[$home_sh]
+		require => Class['java'],
 	}
-
-
 }
