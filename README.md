eef0d54590e558e9fbd85c52a4a9130690e6bc6a
Yinka Erinle <yerinle@gmail.com>
2013-06-19 21:28:25 +0100

    updating README and commenting out code to install grails

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Install GVM (Groovy enVironment Manager) on a vagrant box.
 
-Currently this fails on the first run, but succeeds on the second run.
-
 Tested on:
 
  * Centos 6.4
--- modules/gvm/manifests/system.pp
+++ modules/gvm/manifests/system.pp
@@ -10,12 +10,21 @@ class gvm::system {
 		require => Class['java'],
 	}
 
-	exec { "install-grails":
-		command => "sudo su - vagrant -c 'source /home/vagrant/.gvm/bin/gvm-init.sh && yes Y | gvm install grails'",
-		cwd => "/var/tmp",
-		user => "vagrant",
-		logoutput => true,
-		path => ["/usr/bin", "/usr/sbin", "/bin", "/home/vagrant/.gvm/bin"],
-		require => Exec['install-gvm'],
-	}
+# uncomment to enable gvm to auto answer, this will make every candidate install the latest
+	# $gvm_auto_answer = "/home/vagrant/.gvm/etc/config"
+	# file { $gvm_auto_answer:
+	# 	content => "gvm_auto_answer=true",
+	# 	ensure => present,
+	# }
+
+# uncomment to install the latest version of grails
+	# exec { "install-grails":
+	# 	command => "sudo su - vagrant -c 'source /home/vagrant/.gvm/bin/gvm-init.sh && gvm install grails'",
+	# 	cwd => "/var/tmp",
+	# 	user => "vagrant",
+	# 	logoutput => true,
+	# 	path => ["/usr/bin", "/usr/sbin", "/bin", "/home/vagrant/.gvm/bin"],
+	# 	require => [Exec['install-gvm'], File[$gvm_auto_answer]],
+	# }
+
 }
